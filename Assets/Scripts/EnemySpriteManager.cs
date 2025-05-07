24f9139401bf065823c94f232b2715c16a00a4c4
canaan2000 <canaanhutton@gmail.com>
2025-05-07 18:31:22 -0500

    Added sprite manager

--- Assets/Scripts/EnemySpriteManager.cs
+++ Assets/Scripts/EnemySpriteManager.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpriteManager : MonoBehaviour
 {
+    public List<Sprite> sprites;
+    public Sprite activeSprite;
+    public CombatScript Combat;
+    public Image enemySprite;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,15 @@ public class EnemySpriteManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Combat.combatTrue == true)
+        {
+            enemySprite.enabled = true;
+            activeSprite = sprites[Combat.level - 1];
+            enemySprite.sprite = activeSprite;
+        }
+        else
+        {
+            enemySprite.enabled = false;
+        }
     }
 }
